7ee30fa70140a87f02f6ea66529943faa4090659
pijadewitte-dotcom <pijadewitte@gmail.com>
2026-04-27 15:35:10 +0200

    Bump cache for improved scanner update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'visatracker-github-v11';
+const CACHE = 'visatracker-github-v12';
 const ASSETS = ['./', './index.html', './style.css?v=11', './app.js?v=5', './pdf-export.js?v=5', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
