f8ac6c97735875ecf2eac9f056b43b4fb79680b5
pijadewitte-dotcom <pijadewitte@gmail.com>
2026-04-27 13:43:39 +0200

    Refresh cache for upright receipt export

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'visatracker-github-v8';
-const ASSETS = ['./', './index.html', './style.css?v=8', './app.js?v=3', './pdf-export.js?v=4', './manifest.json', './icon.svg'];
+const CACHE = 'visatracker-github-v9';
+const ASSETS = ['./', './index.html', './style.css?v=9', './app.js?v=3', './pdf-export.js?v=5', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match('./index.html')))); });
