51302369281ac27bcc517deb94c5e3a5695672cc
pijadewitte-dotcom <pijadewitte@gmail.com>
2026-04-27 10:02:42 +0200

    Cache Visa PDF export layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'visatracker-github-v3';
-const ASSETS = ['./', './index.html', './style.css?v=3', './app.js?v=3', './manifest.json', './icon.svg'];
+const CACHE = 'visatracker-github-v4';
+const ASSETS = ['./', './index.html', './style.css?v=4', './app.js?v=3', './pdf-export.js?v=1', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => { const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response; }).catch(() => caches.match('./index.html')))); });
